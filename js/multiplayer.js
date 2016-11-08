17f104dbf5231d176e7787caa180e6a9935ae98f
Alex Francois and Maxwell Cady <alex+maxwell@>
2016-11-08 09:32:04 -0800

    add jquery for moves

---
+++ js/multiplayer.js
@@ -0,0 +1,243 @@
+var uuid = PUBNUB.uuid();
+var turn=2;
+var mySign = '2';
+var playerNumber='2';
+var battleTurn =1;
+function getGameId(){ //Generate game ID
+    if(window.location.search.substring(1).split('?')[0].split('=')[0] !== 'id') {
+      return null;
+    } else {
+      return window.location.search.substring(1).split('?')[0].split('=')[1];
+    }
+  }
+
+
+$(function() {
+// Initialize variables
+  var gameId =  $('#gameId');
+  var gameIdQuery = $('#gameIdQuery');
+  var output = $('#output');
+  var whosTurn = $('#whosTurn');
+  var gameid = '';
+  var rand = (Math.random() * 9999).toFixed(0);
+  gameid = (getGameId()) ? getGameId() : rand;
+  $('#gameid').text(gameid);
+  var channel = 'pokebattle--'+ gameid;
+  var pubnub = PUBNUB.init({
+      subscribe_key: 'sub-c-ffc4b4e4-a451-11e6-9fcd-0619f8945a4f',
+      publish_key: 'pub-c-943a6772-fe6e-435b-a3d1-78ad3ddd1d65',
+      uuid: uuid
+  });
+
+
+  // Button action to go to opponent's url
+  $("#enterGameButton").click(function(event){
+    if($("#inputGameId").val()){
+      // var opponentUrl = 'http://LorenGlenn.github.io/final-pokemon-game/index.html?id=' + $("#inputGameId").val();
+      var opponentUrl = 'file:///Users/Guest/Desktop/final-pokemon-game/index.html?id=' + $("#inputGameId").val();
+      window.location.href=opponentUrl;
+      return false;
+    }
+    else{
+      event.preventDefault();
+    }
+  });
+
+  // add pokemons
+  $("#addPokemon").click(function(){
+    console.log(("#"+activePokemon.name))
+    $(("#"+activePokemon.name).toLowerCase()).addClass("disabled");
+    set();
+
+  });
+
+  pubnub.subscribe({
+     channel: channel,
+     connect: function(){console.log("connected")},
+     presence: function(m) {
+       console.log(m);
+
+       if(m.uuid === uuid && m.action === 'join') {
+          if(m.occupancy < 2) {
+            // modal to select new Game or join host
+            $('#myModal').modal({
+              backdrop: 'static',
+              keyboard: false
+            });
+
+            $("#whosTurn").text('Waiting for your opponent...');
+          } else if(m.occupancy === 2) {
+            mySign = '1';
+          } else if (m.occupancy > 2) {
+            alert('This game already have two players!');
+            $("#pokeSelector").addClass("disabled");
+          }
+        }
+
+       if(m.occupancy === 2) {
+         $("#pokeSelector").removeClass("disabled");
+         console.log("Start the game!")
+         startNewGame(mySign);
+       }
+       (mySign==1)? $('#player').text("2"):$('#player').text("1");
+       (mySign==1)? playerNumber=2:playerNumber=1;
+       // Presence Stuff
+       if(document.querySelector('.presence')) {
+         showPresenceExamples(m);
+       }
+
+     },
+     callback: function(m) {
+       if(Game.state=="Menu"){
+         changeTurn(m.player);
+         (turn==1) ? Player1.turn++ : Player2.turn++;
+         $("#player1Turn").text(Player1.turn);
+         $("#player2Turn").text(Player2.turn);
+         if(Game.currentTurn>1){
+           console.log("received " + m.pokemon.name)
+           $(("#"+m.pokemon.name).toLowerCase()).addClass("disabled");
+
+           (m.pokemon) ? eval("Player"+turn).pokemons.push(m.pokemon): console.log("got squat");
+
+           (Game.currentTurn>1 && turn==1) ? $("#selectedPokemonsP"+turn).append("<img src='" + eval(eval("Player"+turn).pokemons[Player1.turn-2]).frontSprite + "'>") : console.log("wont append the pokemon1");
+           (Game.currentTurn>1 && turn==2) ? $("#selectedPokemonsP"+turn).append("<img src='" + eval(eval("Player"+turn).pokemons[Player2.turn-2]).frontSprite + "'>") : console.log("wont append the pokemon2");
+           if(Player1.pokemons.length == 1 ) {
+             $("#battleReadyP1").removeClass("btn-primary");
+             $("#battleReadyP1").addClass("btn-success");
+             checkReady();
+           }
+           if(Player2.pokemons.length == 1) {
+             $("#pokeSelector").addClass("disabled");
+             $("#battleReadyP2").removeClass("btn-primary");
+             $("#battleReadyP2").addClass("btn-success");
+             checkReady();
+           }
+          }
+        }
+      if(Game.state=="Battle"){
+        displaySprite(0,0);
+        var roundBegin = false;
+        $(".attack").click(function(){
+          eval("Player"+playerNumber).currentAction = $(this).val();
+          if (Player1.currentAction !== false && Player2.currentAction !== false) {
+             roundBegin = true;
+          }
+        });
+        if (roundBegin){
+          console.log("Round begins!")
+
+          if(parseInt(eval("Player"+playerNumber).currentAction)){
+            eval("Player"+playerNumber).currentPokemon = parseInt(eval("Player"+playerNumber).currentAction);
+            eval("Player"+playerNumber).currentAction = "skip";
+            displaySprite(eval("Player"+playerNumber).currentPokemon);
+          }
+          else{
+            if(Player2.pokemons[Player2.currentPokemon].speed > Player1.pokemons[Player1.currentPokemon].speed){
+              battleTurn=2;
+              attack(Player2.currentAction);
+              attack(Player1.currentAction);
+            }else{
+              battleTurn=1;
+              attack(Player1.currentAction);
+              attack(Player2.currentAction);
+            }
+        }
+          eval("Player"+playerNumber).currentAction = "";
+
+        }
+
+        Game.state="Done";
+      }
+    },
+   });
+
+
+function attack(attackName){
+  if(eval("Player"+playerNumber).currentAction !=="skip"){
+    var attacker= (Player1.isPlayerTurn) ? (Player1.pokemons[Player1.currentPokemon]) : (Player2.pokemons[Player2.currentPokemon])
+    var defender= (Player1.isPlayerTurn) ? (Player2.pokemons[Player2.currentPokemon]) : (Player1.pokemons[Player1.currentPokemon])
+    var damage = ((attacker.attack + eval(attackName).power) - defender.defense);
+    console.log("attacker is: "+attacker + " Defender is: "+ defender);
+    if(attacker.type == defender.weakAgainst){
+      damage *= 2;
+    } else if (attackName.type == defender.strongAgainst){
+      damage *= .5;
+    }
+    defender.hp -= damage;
+  }
+}
+
+function displaySprite(index1, index2){
+
+  eval("Player"+playerNumber).currentPokemon = index1;
+  $("#Player1Fighter").append("<img src='" + eval("Player"+playerNumber).pokemons[index1].backSprite + "' width='150px'><br>");
+  $("#Player2Fighter").append("<img src='" + eval("Player"+mySign).pokemons[index2].frontSprite + "' width='90px'><br>");
+
+  $("#Pokemon1Name").html(eval("Player"+playerNumber).pokemons[index1].name);
+  $("#Pokemon2Name").html(eval("Player"+mySign).pokemons[index1].name);
+
+  var hpToPercent = function(hp, maxHP) {
+    return (hp / maxHP) * 100;
+  }
+
+  hpToPercent()
+
+  // $(".pokemon1HP").html(eval("Player"+playerNumber).pokemons[index1].hp + " HP");
+  // $(".pokemon2HP").html(eval("Player"+mySign).pokemons[index1].hp + " HP");
+
+  $(".move1Name").html(eval("Player"+playerNumber).pokemons[index1].moves[0].name);
+  $(".move2Name").html(eval("Player"+playerNumber).pokemons[index1].moves[1].name);
+
+  $(".move1Description").html(eval("Player"+playerNumber).pokemons[index1].moves[0].description);
+  $(".move2Description").html(eval("Player"+playerNumber).pokemons[index1].moves[1].description);
+
+  $(".move1Power").html(eval("Player"+playerNumber).pokemons[index1].moves[0].power);
+  $(".move2Power").html(eval("Player"+playerNumber).pokemons[index1].moves[1].power);
+}
+
+ function publishPosition(player,pokemonChosen) {
+   pubnub.publish({
+     channel: channel,
+     message: {player: player, pokemon: pokemonChosen},
+     callback: function(m){
+       console.log(m);
+     }
+   });
+  }
+  function set() {
+    if (turn !== mySign) return;
+    publishPosition(mySign,activePokemon);
+
+  }
+  function startNewGame(mySign){
+    turn =1;
+    console.log("Starting new game!")
+    console.log("My sign is: "+mySign)
+    publishPosition(mySign);
+  }
+
+  function changeTurn(player) {
+      Game.currentTurn++;
+      turn = (turn === '1') ? '2' : '1';
+      console.log("changing turns, "+ turn);
+      $("#whosTurn").text((turn === mySign) ? 'Your turn' : 'Your opponent\'s turn');
+      (turn==mySign) ? $("#pokeSelector").removeClass("disabled") : $("#pokeSelector").addClass("disabled");
+      $("#currentTurn").text(Game.currentTurn);
+      if(Player1.isPlayerTurn) {
+        Player1.isPlayerTurn = false;
+        Player2.isPlayerTurn = true;
+      } else {
+        Player2.isPlayerTurn = false;
+        Player1.isPlayerTurn = true;
+      }
+    }
+
+    function checkReady(){
+      if($('#battleReadyP1').hasClass("btn-success") && $('#battleReadyP2').hasClass("btn-success")) {
+        $('#container').hide();
+        $("#battleContainer").show();
+        Game.state="Battle";
+      }
+    }
+
+})
---
+++ js/scripts.js
@@ -0,0 +1,329 @@
+var activePokemon;
+
+var leafBlade = {
+  name: "Leaf Blade",
+  type: "Grass",
+  power: 20,
+  description: "A leaf attack."
+}
+
+var megaDrain = {
+  name: "Mega Drain",
+  type: "Grass",
+  power: 10,
+  description: "User gains half the HP inflicted on opponent."
+}
+
+var ember = {
+  name: "Ember",
+  type: "Fire",
+  power: 10,
+  description: "Inflicts burn on opponent."
+}
+
+var heatWave = {
+  name: "Heat Wave",
+  type: "Fire",
+  power: 20,
+  description: "Burning wave of heat."
+}
+
+var bubble = {
+  name: "Bubble",
+  type: "Water",
+  power: 10,
+  description: "Lowers speed stat of defending Pokemon"
+}
+
+var waterfall = {
+  name: "Waterfall",
+  type: "Water",
+  power: 20,
+  description: "A powerful waterfall move"
+}
+
+var thunderwave = {
+  name: "Thunder Wave",
+  type: "Electric",
+  power: 10,
+  description: "An electric charge that paralyzes the opponent."
+}
+
+var electroball = {
+  name: "Electroball",
+  type: "Electric",
+  power: 20,
+  description: "A strong electric energy ball that is thrown at the opponent."
+}
+
+var screech = {
+  name: "Screech",
+  type: "Normal",
+  power: 10,
+  description: "A harsh scream that lowers the opponents defense."
+}
+
+var payday = {
+  name: "Pay Day",
+  type: "Normal",
+  power: 20,
+  description: "Meowth's signature attack that deals damage."
+}
+
+var psychic = {
+  name: "Psychic",
+  type: "Psychic",
+  power: 20,
+  description: "Deals strong psychic damage to the opponent"
+}
+
+var barrier = {
+  name: "Barrier",
+  type: "Psychic",
+  power: 0,
+  description: "Greatly increases defense stat"
+}
+
+var psyshock = {
+  name: "Barrier",
+  type: "Psychic",
+  power: 20,
+  description: "Powerful psychic attack"
+}
+
+var torment = {
+  name: "Torment",
+  type: "Psychic",
+  power: 10,
+  description: "Lowers target's attack stat"
+}
+
+var sing = {
+  name: "Sing",
+  type: "Normal",
+  power: 10,
+  description: "Puts opponent to sleep"
+}
+
+var rollout = {
+  name: "Rollout",
+  type: "Normal",
+  power: 20,
+  description: "Attack move that deals heavy damage"
+}
+
+var karatechop = {
+  name: "Karate Chop",
+  type: "Fighting",
+  power: 20,
+  description: "Attack move that deals damage"
+}
+
+var closecombat = {
+  name: "Close Combat",
+  type: "Fighting",
+  power: 30,
+  description: "Deals very heavy damage but user hurts self"
+}
+
+var bonerush = {
+  name: "Bone Rush",
+  type: "Ground",
+  power: 10,
+  description: "Increases user speed and deals small amount of damage"
+}
+
+var earthquake = {
+  name: "Earthquake",
+  type: "Ground",
+  power: 20,
+  description: "Ground move that deals heavy damage to opponent"
+}
+
+var bulbasaur = {
+  name: "Bulbasaur",
+  type: "Grass",
+  hp: 120,
+  attack: 60,
+  defense: 80,
+  speed: 40,
+  moves: [leafBlade, megaDrain],
+  backSprite: "http://www.pokestadium.com/sprites/xy/back/bulbasaur.gif",
+  frontSprite: "http://www.pokestadium.com/sprites/xy/bulbasaur.gif",
+  weakAgainst: "Fire",
+  strongAgainst: "Ground"
+}
+
+var charmander = {
+  name: "Charmander",
+  type: "Fire",
+  hp: 70,
+  attack: 100,
+  defense: 50,
+  speed: 80,
+  moves: [ember, heatWave],
+  backSprite: "http://www.pokestadium.com/sprites/xy/back/charmander.gif",
+  frontSprite: "http://www.pokestadium.com/sprites/xy/charmander.gif",
+  weakAgainst: "Water",
+  strongAgainst: "Grass"
+}
+
+var squirtle = {
+  name: "Squirtle",
+  type: "Water",
+  hp: 80,
+  attack: 60,
+  defense: 100,
+  speed: 60,
+  moves: [bubble, waterfall],
+  backSprite: "http://www.pokestadium.com/sprites/xy/back/squirtle.gif",
+  frontSprite: "http://www.pokestadium.com/sprites/xy/squirtle.gif",
+  weakAgainst: "Electric",
+  strongAgainst: "Fire"
+}
+
+var pikachu = {
+  name: "Pikachu",
+  type: "Electric",
+  hp: 60,
+  attack: 80,
+  defense: 60,
+  speed: 100,
+  moves: [thunderwave, electroball],
+  backSprite: "http://www.pokestadium.com/sprites/xy/back/pikachu.gif",
+  frontSprite: "http://www.pokestadium.com/sprites/xy/pikachu.gif",
+  weakAgainst: "Ground",
+  strongAgainst: "Water"
+}
+
+var meowth = {
+  name: "Meowth",
+  type: "Normal",
+  hp: 80,
+  attack: 70,
+  defense: 60,
+  speed: 90,
+  moves: [screech, payday],
+  backSprite: "http://www.pokestadium.com/sprites/xy/back/meowth.gif",
+  frontSprite: "http://www.pokestadium.com/sprites/xy/meowth.gif",
+  weakAgainst: "Fighting",
+  strongAgainst: "Psychic"
+}
+
+var mew = {
+  name: "Mew",
+  type: "Psychic",
+  hp: 100,
+  attack: 90,
+  defense: 80,
+  speed: 80,
+  moves: [psychic, barrier],
+  backSprite: "http://www.pokestadium.com/sprites/xy/back/mew.gif",
+  frontSprite: "http://www.pokestadium.com/sprites/xy/mew.gif",
+  weakAgainst: "Normal",
+  strongAgainst: "Fighting"
+}
+
+var abra = {
+  name: "Abra",
+  type: "Psychic",
+  hp: 100,
+  attack: 80,
+  defense: 90,
+  speed: 80,
+  moves: [psyshock],
+  backSprite: "http://www.pokestadium.com/sprites/xy/back/abra.gif",
+  frontSprite: "http://www.pokestadium.com/sprites/xy/abra.gif",
+  weakAgainst: "Normal",
+  strongAgainst: "Fighting"
+}
+
+var jigglypuff = {
+  name: "Jigglypuff",
+  type: "Normal",
+  hp: 120,
+  attack: 40,
+  defense: 80,
+  speed: 60,
+  moves: [sing, rollout],
+  backSprite: "http://www.pokestadium.com/sprites/xy/back/jigglypuff.gif",
+  frontSprite: "http://www.pokestadium.com/sprites/xy/jigglypuff.gif",
+  weakAgainst: "Fighting",
+  strongAgainst: "Psychic"
+}
+
+var mankey = {
+  name: "Mankey",
+  type: "Fighting",
+  hp: 70,
+  attack: 120,
+  defense: 50,
+  speed: 60,
+  moves: [karatechop, closecombat],
+  backSprite: "http://www.pokestadium.com/sprites/xy/back/mankey.gif",
+  frontSprite: "http://www.pokestadium.com/sprites/xy/mankey.gif",
+  weakAgainst: "Psychic",
+  strongAgainst: "Normal"
+}
+
+var cubone = {
+  name: "Cubone",
+  type: "Ground",
+  hp: 60,
+  attack: 90,
+  defense: 110,
+  speed: 40,
+  moves: [bonerush, earthquake],
+  backSprite: "http://www.pokestadium.com/sprites/xy/back/cubone.gif",
+  frontSprite: "http://www.pokestadium.com/sprites/xy/cubone.gif",
+  weakAgainst: "Grass",
+  strongAgainst: "Electric"
+}
+// Player1.pokemons[Player1.currentPokemon].moves[0].name
+var Player1 = {
+  pokemons : [],
+  turn:0,
+  isPlayerTurn: true,
+  currentPokemon: 0,
+  currentAction: false
+}
+
+var Player2 = {
+  pokemons :[],
+  turn:0,
+  isPlayerTurn: false,
+  currentPokemon: 0,
+  currentAction: false
+}
+var Game = {
+  currentTurn:-1,
+  state:"Menu"
+}
+
+$(function() {
+  activePokemon="cubone";
+  var p1SelectedPokemons=0;
+  var p2SelectedPokemons=0;
+  $("#pokeSelector img").click(function(){
+    activePokemon=eval($(this).attr("id"));
+  });
+
+  $(".icon").click(function(){
+    // console.log(activePokemon);
+    // console.log(activePokemon.name);
+    $("#activePokemon").html("<img src='" + activePokemon.frontSprite + "'>");
+    $("#pokemonName").html(activePokemon.name);
+    $("#hp").html(activePokemon.hp);
+    $("#attack").html(activePokemon.attack);
+    $("#defense").html(activePokemon.defense);
+    $("#speed").html(activePokemon.speed);
+    $("#weakness").html(activePokemon.weakAgainst);
+    $("#resistance").html(activePokemon.strongAgainst);
+    $("#moves").empty();
+    activePokemon.moves.forEach(function(move) {
+      $("#moves").append("<li>" + move.name + "</li>")
+    })
+  })
+
+
+})
