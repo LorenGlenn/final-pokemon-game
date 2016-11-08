1c40bac768fd4b65644eceb5979a63c7ed8f32d6
Diego Suarez and Loren Glenn <loren@>
2016-11-08 15:28:03 -0800

    working on switching out pokemon

--- js/multiplayer.js
+++ js/multiplayer.js
@@ -57,19 +57,15 @@ $(function() {
     var newPokemon;
     $("#startAttack").removeClass("disabled");
     eval("Player"+playerNumber).currentAction = $(this).attr("value");
-    if(parseInt(eval("Player"+playerNumber).currentAction)){
+    if(parseInt(eval("Player"+playerNumber).currentAction)+1){
       newPokemon = parseInt(eval("Player"+playerNumber).currentAction);
       eval("Player"+playerNumber).currentAction = "skip";
     }
     else{ // get battleTurn
       if(Player2.pokemons[Player2.currentPokemon].speed > Player1.pokemons[Player1.currentPokemon].speed){
         battleTurn=2;
-        // attack(Player2.currentAction);
-        // attack(Player1.currentAction);
       }else{
         battleTurn=1;
-        // attack(Player1.currentAction);
-        // attack(Player2.currentAction);
       }
       if(eval("Player"+playerNumber).currentAction !=="skip"){  // get damage
         var attacker= (Player1.isPlayerTurn) ? (Player1.pokemons[Player1.currentPokemon]) : (Player2.pokemons[Player2.currentPokemon])
@@ -129,19 +125,18 @@ $(function() {
          $("#player1Turn").text(Player1.turn);
          $("#player2Turn").text(Player2.turn);
          if(Game.currentTurn>1){
-           console.log("received " + m.pokemon.name)
            $(("#"+m.pokemon.name).toLowerCase()).addClass("disabled");
 
            (m.pokemon) ? eval("Player"+turn).pokemons.push(m.pokemon): console.log("got squat");
 
            (Game.currentTurn>1 && turn==1) ? $("#selectedPokemonsP"+turn).append("<img src='" + eval(eval("Player"+turn).pokemons[Player1.turn-2]).frontSprite + "'>") : console.log("wont append the pokemon1");
            (Game.currentTurn>1 && turn==2) ? $("#selectedPokemonsP"+turn).append("<img src='" + eval(eval("Player"+turn).pokemons[Player2.turn-2]).frontSprite + "'>") : console.log("wont append the pokemon2");
-           if(Player1.pokemons.length == 1 ) {
+           if(Player1.pokemons.length == 3 ) {
              $("#battleReadyP1").removeClass("btn-primary");
              $("#battleReadyP1").addClass("btn-success");
              checkReady();
            }
-           if(Player2.pokemons.length == 1) {
+           if(Player2.pokemons.length == 3) {
              $("#pokeSelector").addClass("disabled");
              $("#battleReadyP2").removeClass("btn-primary");
              $("#battleReadyP2").addClass("btn-success");
@@ -149,18 +144,58 @@ $(function() {
            }
           }
         }
+        console.log("Player: "+m.player + " changing pokemon to: " + m.pokemonChange + " damage: "+ m.damage + " statusEffect: "+ m.statusEffect);
         (m.player===1) ? P1damage=m.damage : P2damage=m.damage;
+        if(!P1damage){
+          P1damage=0;
+        }
+        if(!P2damage){
+          P2damage=0;
+        }
         Game.player1Attack=P1damage;
         Game.player2Attack=P2damage;
-        // console.log("new message1 player: "+ m.player +" pokemonIndex: "+ m.pokemonChange +" damage: "+ m.damage + " status: " +m.statusEffect + " battleTurn "+ m.battleTurn)
-      if(Game.state=="Battle"){
 
-        displaySprite(0,0);
+        if(m.player===1 && m.pokemonChange+1) {
+          Player1.currentPokemon = m.pokemonChange;
+          Game.player1Attack = 1;
+        }
+
+        if (m.player===2 && m.pokemonChange+1)
+        {
+          Player2.currentPokemon = m.pokemonChange;
+          Game.player2Attack = 1;
+        }
+        // console.log("new message1 player: "+ m.player +" pokemonIndex: "+ m.pokemonChange +" damage: "+ m.damage + " status: " +m.statusEffect + " battleTurn "+ m.battleTurn)
+      if(m.GameStatus=="Attacking"){
+        console.log("attacking on both screens")
+        if(eval("Player"+playerNumber).currentAction=="skip"){
+          displaySprite(eval("Player"+playerNumber).currentPokemon);
+        }
+        publishReset();
+      }
+      if(Game.state=="Battle"){
+        console.log("Test2"+eval("Player"+playerNumber).currentPokemon)
+        displaySprite(0);
         var roundBegin = false;
         $("#startAttack").click(function(){
-          if(Game.player1Attack!== undefined && Game.player2Attack!== undefined){
-            Player1.pokemons[Player1.currentPokemon].hp -= Game.player2Attack;
-            Player2.pokemons[Player2.currentPokemon].hp -= Game.player1Attack;
+          if(Game.player1Attack!== 0 && Game.player2Attack!== 0){
+            beginAttack();
+            // console.log("Attacking!");
+            //
+            // else{
+            //   console.log("computing attack")
+            //   if(Player2.pokemons[Player2.currentPokemon].speed > Player1.pokemons[Player1.currentPokemon].speed){
+            //     Player2.pokemons[Player2.currentPokemon].hp -= Game.player1Attack;
+            //     // pokemonDeathCheck();
+            //     // playerWinCheck();
+            //   }else {
+            //     Player1.pokemons[Player1.currentPokemon].hp -= Game.player2Attack;
+            //     // pokemonDeathCheck();
+            //     // playerWinCheck();
+            //   }
+            //}
+
+
           }
 
         });
@@ -168,15 +203,21 @@ $(function() {
 
         Game.state="Done";
       }
+      if(m.reset=="Reset"){
+        Game.player1Attack= 0;
+        Game.player2Attack= 0;
+        P1damage=0;
+        P2damage=0;
+      }
     },
    });
 
 
-function displaySprite(index1, index2){
+function displaySprite(index1){
 
   eval("Player"+playerNumber).currentPokemon = index1;
-  $("#Player1Fighter").append("<img src='" + eval("Player"+playerNumber).pokemons[index1].backSprite + "' width='150px'><br>");
-  $("#Player2Fighter").append("<img src='" + eval("Player"+mySign).pokemons[index2].frontSprite + "' width='90px'><br>");
+  $("#Player1Fighter").html("<img src='" + eval("Player"+playerNumber).pokemons[index1].backSprite + "' width='150px'><br>");
+  $("#Player2Fighter").html("<img src='" + eval("Player"+mySign).pokemons[index1].frontSprite + "' width='90px'><br>");
 
   $("#Pokemon1Name").html(eval("Player"+playerNumber).pokemons[index1].name);
   $("#Pokemon2Name").html(eval("Player"+mySign).pokemons[index1].name);
@@ -206,6 +247,25 @@ function displaySprite(index1, index2){
    pubnub.publish({
      channel: channel,
      message: {player: player, pokemon: pokemonChosen},
+     callback: function(m){
+       console.log(m);
+     }
+   });
+  }
+  function beginAttack(player,pokemonChosen) {
+    pubnub.publish({
+      channel: channel,
+      message: {GameStatus: "Attacking"},
+      callback: function(m){
+        console.log(m);
+      }
+    });
+   }
+
+ function publishReset(player,pokemonChosen) {
+   pubnub.publish({
+     channel: channel,
+     message: {reset:"Reset"},
      callback: function(m){
        console.log(m);
      }
